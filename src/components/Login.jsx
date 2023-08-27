1d2cdb106601a487e5722927e01ce69fc600db85
ARUN L <108601293+Arun100203@users.noreply.github.com>
2023-08-27 10:58:48 +0000

    change UI of login and register page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { auth, signInWithGoogle } from "./firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
-import "./login.css";
+
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -39,37 +39,82 @@ function Login() {
   }
   return (
     
-    <div className="login">
-      <div className="login__container">
-        <input
-          type="text"
-          className="login__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <input
-          type="password"
-          className="login__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button
-          className="login__btn"
-          onClick={fun2}
-        >
-          Login
-        </button>
-        <button className="login__btn login__google" onClick={fun}>
-          Login with Google
-        </button>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <a href="https://amazon-clone-react-lovat.vercel.app/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                <img
+                src='./images/amazon-ar21.svg'
+                className='h-20 mx-auto w-auto'
+                />
+            </a>
+          </div>
   
-        <div>
-          Don't have an account? <Link to="/register">Register</Link> now.
+          <div className="mt-10 border-2 p-6  sm:mx-auto sm:w-full sm:max-w-sm space-y-6">
+            
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={(e)=>setEmail(e.target.value)}
+                    autoComplete="email"
+                    value={email}
+                    placeholder='  enter your name'
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-yellow-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                  </label>
+                  
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={password}
+                    placeholder='  enter your password'
+                    autoComplete="current-password"
+                    onChange={(e)=>setPassword(e.target.value)}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-yellow-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  onClick={fun2}
+                  className="flex w-full justify-center rounded-md transition-all duration-500 bg-yellow-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  Login 
+                </button>
+                <button 
+                className="flex w-full mt-4 justify-center rounded-md transition-all duration-500 bg-yellow-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={fun}>
+                  Login with Google
+                </button>
+              </div>
+           
+  
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Not a member?{' '}
+              <Link to="/register" className="text-yellow-400  transition-all duration-500 hover:text-orange-400 hover:font-semibold">
+                 Register now
+              </Link>
+            </p>
+          </div>
         </div>
-      </div>
-    </div>
   );
 }
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -28,43 +28,102 @@ function Register() {
     if (loading) return;
   }, [user, loading]);
   return (
-    <div className="register">
-      <div className="register__container">
-        <input
-          type="text"
-          className="register__textBox"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Full Name"
-        />
-        <input
-          type="text"
-          className="register__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <input
-          type="password"
-          className="register__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button className="register__btn" onClick={register}>
-          Register
-        </button>
-        <button
-          className="register__btn register__google"
+    <div className="flex  min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <a href="https://amazon-clone-react-lovat.vercel.app/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+          <img
+          src='./images/amazon-ar21.svg'
+          className='h-20 mx-auto w-auto'
+          />
+      </a>
+    </div>
+
+    <div className="mt-10 border-2 p-6 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" action="#" method="POST">
+        <div>
+      <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Username
+          </label>
+      <div className="mt-2">
+            <input
+              id="Username"
+              name="Username"
+              type="Username"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              placeholder='  Enter your Username'
+              autoComplete="current-Username"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-yellow-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+            />
+          </div>
+          </div>
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              placeholder='  Enter your name'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}       
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-yellow-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              Password
+            </label>
+            
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder='  Enter your password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} 
+              autoComplete="current-password"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-yellow-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            onClick={register}
+            className="flex w-full justify-center transition-all duration-500 rounded-md bg-yellow-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Register now
+          </button>
+          <button
+          className="flex w-full mt-4 justify-center transition-all duration-500 rounded-md bg-yellow-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           onClick={signInWithGoogle}
         >
           Register with Google
         </button>
-        <div>
-          Already have an account? <Link to="/">Login</Link> now.
         </div>
-      </div>
+      </form>
+
+      <p className="mt-10 text-center text-sm text-gray-500">
+        Already a member? {' '}
+        <Link to="/" className="text-yellow-400  transition-all duration-500 hover:text-orange-400 hover:font-semibold">
+          Login 
+        </Link>
+      </p>
     </div>
+  </div>
   );
 }
 export default Register;
